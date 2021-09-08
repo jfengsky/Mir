f6fb75225143aca400f90f4e182717ea168f2bb9
jiangfeng <jiangfeng@yunlizhihui.com>
2021-09-08 09:55:31 +0800

    暂存

--- src/pages/admin/Livingedit.js
+++ src/pages/admin/Livingedit.js
@@ -6,45 +6,75 @@ import Style from "../../assets/css/admin.less";
 
 const { Search } = Input;
 
+const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+class Player {
+    level = 0;
+    minAt = 0;
+    maxAt = 0;
+}
+
+
 const LivingeditContent = inject("store")(
-  observer((props) => {
-    const {
-      store: {
-        GlobalStore: { lang },
-      },
-    } = props;
+    observer((props) => {
+        const {
+            store: {
+                GlobalStore: { lang },
+            },
+        } = props;
 
-    const changeSearch = (e) => {
-      console.log(e.target.value);
-    };
+        const changeSearch = (e) => {
+            console.log(e.target.value);
+        };
 
-    return (
-      <>
-        <Row>
-          <Col flex="200px">
-            <Search
-              placeholder={lang.searchText}
-              onChange={changeSearch}
-              allowClear
-            />
-          </Col>
-          <Col flex="auto"></Col>
-          <Col flex="100px" className={Style.alignright}>
-            <Button type="primary">{lang.addMenu}</Button>
-          </Col>
-        </Row>
-        <Divider />
-      </>
-    );
-  })
+        useEffect(() => {
+            const minAtk = 4;
+            const maxAtk = 8;
+
+            const minDef = 2;
+            const maxDef = 4;
+
+            let minAttack = minAtk - maxDef;
+            let maxAttack = maxAtk - minDef;
+
+            let player = new Player();
+            console.log(player)
+
+            setInterval(() => {
+
+                console.log(random(minAttack, maxAttack))
+            }, 1000)
+        }, [])
+
+        return (
+            <>
+                <Row>
+                    <Col flex="200px">
+                        <Search
+                            placeholder={lang.searchText}
+                            onChange={changeSearch}
+                            allowClear
+                        />
+                    </Col>
+                    <Col flex="auto"></Col>
+                    <Col flex="100px" className={Style.alignright}>
+                        <Button type="primary">{lang.addMenu}</Button>
+                    </Col>
+                </Row>
+                <Divider />
+            </>
+        );
+    })
 );
 
 const Livingedit = () => {
-  return (
-    <AdminLayout>
-      <LivingeditContent />
-    </AdminLayout>
-  );
+    return (
+        <AdminLayout>
+            <LivingeditContent />
+        </AdminLayout>
+    );
 };
 
 export default Livingedit;
